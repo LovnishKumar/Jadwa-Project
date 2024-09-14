85746993bae2c2227f2324f8094fe7de373f45b2
Lovnish Kumar <Lovnish390@gmail.com>
2024-09-14 21:07:45 +0530

    progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import './Navbar.css'
 import Logo from '../assets/logo.png'
+import Profile_pic from '../assets/profile.png'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
@@ -9,8 +11,8 @@ const Navbar = () => {
 
 <div class="flex h-screen bg-gray-100">
     <div class="hidden md:flex flex-col w-64 bg-gray-800">
-        <div class="flex items-center justify-center h-16 bg-white">
-            <span><img width={'90px'} src={Logo} alt="" /></span>
+        <div class="flex items-center ps-10  h-16 bg-white">
+            <span><img width={'85px'} src={Logo} alt="" /></span>
         </div>
         <div class="flex flex-col flex-1 overflow-y-auto">
             <nav class="flex-1 px-2 py-4 bg-gray-100">
@@ -41,14 +43,14 @@ const Navbar = () => {
             </nav>
         </div>
     </div>
-    <div class="flex flex-col flex-1 overflow-y-auto">
-        <div class="flex items-center justify-between h-16 bg-white border-b border-gray-200">
+    <div class="flex flex-col flex-1 justify-between overflow-y-auto">
+        <div class="flex items-center h-16 bg-white border-b border-gray-200">
             <div class="flex items-center px-4">
-        
+        <div>
     <Menu as="div" className="relative z-10 inline-block text-left ">
       <div>
-        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-          Options
+        <MenuButton className="inline-flex justify-between gap-x-1.5 w-56 py-3 rounded-xl text-gray-400 bg-[#fafafb] px-3 py-2 text-sm font-semibold  shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          Afterglow
           <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
         </MenuButton>
       </div>
@@ -61,9 +63,9 @@ const Navbar = () => {
           <MenuItem>
             <a
               href="#"
-              className="block px-4 py-2  text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
             >
-              ssssssssss
+              Option 1
             </a>
           </MenuItem>
           <MenuItem>
@@ -71,7 +73,7 @@ const Navbar = () => {
               href="#"
               className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
             >
-              Support
+              Option 2
             </a>
           </MenuItem>
           <MenuItem>
@@ -79,29 +81,81 @@ const Navbar = () => {
               href="#"
               className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
             >
-              License
+              Option 3
             </a>
           </MenuItem>
-          <form action="#" method="POST">
+          <form action="/">
             <MenuItem>
               <button
                 type="submit"
                 className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
               >
-                Sign out
+                Option 4
               </button>
             </MenuItem>
           </form>
         </div>
       </MenuItems>
     </Menu>
-  
-                <input class="mx-4 w-full border rounded-md px-4 py-2" type="text" placeholder="Search"/>
-            </div>
-            <div class="flex items-center pr-4">
 
+
+    <Menu as="div" className="relative z-10 inline-block text-left ">
+      <div className=' w-56 py-3  text-gray-800 px-3 py-2  text-xs'>
+        <span className='inline-flex'><h6 className='me-2 text-gray-400'>Scenario</h6>
+        <MenuButton className="box  inline-flex">
+          Default
+          <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
+        </MenuButton>
+        </span>
+      </div>
+
+      <MenuItems
+        transition
+        className="absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+      >
+        <div className="py-1">
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-xs text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            >
+              Option 1
+            </a>
+          </MenuItem>
+         
+        </div>
+      </MenuItems>
+    </Menu>
+    </div>
+    
+    <div class="w-52">
+  <div class="relative flex left-40 z-10 align-middle">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="absolute w-5 h-5 top-2.5 left-2.5 text-slate-400">
+      <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clip-rule="evenodd" />
+    </svg>
+ 
+    <input
+    class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border-none pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none "
+    placeholder="Search...." 
+    />
+    
+  </div>
+
+
+
+</div>
+  <div className="profile ms-20 block justify-end flex">
+  <div className="profile_pic">
+    <img src={Profile_pic} alt="profile_pic" />
+  </div>
+  <div className='ms-3 text-gray-400'>
+    <h4 className='text-gray-700'>Robert Fox</h4>
+    <h6 className='text-gray-400'>Finance Manager</h6>
+  </div>
+  </div>
                
             </div>
+            
         </div>
         <div class="p-4">
             <h1 class="text-2xl font-bold">Welcome to my dashboard!</h1>
@@ -110,6 +164,7 @@ const Navbar = () => {
     </div>
     
 </div>
+
 
     </div>
   )
